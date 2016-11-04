d896fb387d9e549f4367a7e6628616e09d915dc8
zhanghua <zhanghua8003@gmail.com>
2016-11-04 13:42:50 +0800

    我的视图

--- src/components/pageHomebox.vue
+++ src/components/pageHomebox.vue
@@ -8,8 +8,11 @@
                     <el-button type="primary" size="small"><i class="el-icon-edit"></i></el-button>
                     
                     <el-dropdown-menu slot="dropdown" class="home-card-dropdown">
-                        <el-dropdown-item v-for="item in statesList">
-                            {{item}}
+                        <el-dropdown-item >
+                            <div v-on:click="setSort('all')">所有状态</div>
+                        </el-dropdown-item>
+                        <el-dropdown-item v-for="item in statesList" >
+                            <div v-on:click="setSort(item)">{{item}}</div>
                         </el-dropdown-item>
                         
                     </el-dropdown-menu>
@@ -17,14 +20,22 @@
                 <router-link :to="boxData.more" class="link-item" v-if="boxData.more"><el-button type="primary" size="small" ><i class="el-icon-more"></i></el-button></router-link>
             </div>
         </div>
-        <div v-for="list in boxData.listData" class="text item">
-            #{{list.id}} <strong class="">{{list.urgent}}</strong> <router-link :to="list.url">{{list.name}}</router-link> (<el-tag class="ml5 mr5">{{list.status}}</el-tag> <strong v-if="list.endTime">{{list.endTime}}</strong>)
+        <div v-for="list in boxData.listData" class="text item" v-if="statusShow=='all' || statusShow==list.status">
+            #{{list.id}} <strong class="">{{list.urgent}}</strong> <router-link :to="list.url">{{list.name}}</router-link> (<el-tag type="warning" class="ml5 mr5">{{list.status}}</el-tag> <strong v-if="list.endTime">{{list.endTime}}</strong> )
         </div>
     </el-card>
 </template>
 
 <style lang="less">
-    
+    .home-card{margin: 20px 10px;
+        .el-card__header{padding:5px 10px; font-weight: bold; font-size: 14px; }
+        .el-card__body{font-size: 14px; padding: 10px; line-height: 40px;
+            a{color: #333; text-decoration: none;
+                &:hover{color: #f60;}
+            }
+        }
+        &-dropdown{font-size: 12px; padding: 0;}
+    }
   
 </style>
 
@@ -35,12 +46,17 @@
 export default {
     data () {
         return {
-          
+            statusShow:'all'
         }
     },
     props:['boxData'],
     mounted(){
 
+    },
+    methods:{
+        setSort:function(text){
+            this.statusShow = text
+        }
     },
     computed:{
         statesList:function(){
--- src/view/home.vue
+++ src/view/home.vue
@@ -15,11 +15,7 @@
 
 <style lang="less" >
    
-    .home-card{margin: 20px 10px;
-        .el-card__header{padding:5px 10px; font-weight: bold; font-size: 14px; }
-        .el-card__body{font-size: 12px; padding: 10px; line-height: 30px;}
-        &-dropdown{font-size: 12px; padding: 0;}
-    }
+    
 </style>
 
 <script>
@@ -32,10 +28,10 @@ export default {
     return{
         waitMySolveBug: {"title":"待我解决的BUG","more":"bug","listData":[
                 {"id":"14816","urgent":"","name":"外包bug阿斯顿发送到发生","status":"待解决","endTime":"2016-10-29","url":"bug/detail?id=14816&p=32"},
+                {"id":"14816","urgent":"","name":"  bug添加一个bug责任人申诉的额外流程 ","status":"已完成","endTime":"2016-10-29","url":"bug/detail?id=14816&p=32"},
+                {"id":"14816","urgent":"","name":" 在项目组需求统计和项目需求统计 新增统计字段","status":"待解决","endTime":"2016-10-29","url":"bug/detail?id=14816&p=32"},
                 {"id":"14816","urgent":"","name":"外包bug阿斯顿发送到发生","status":"待解决","endTime":"2016-10-29","url":"bug/detail?id=14816&p=32"},
-                {"id":"14816","urgent":"","name":"外包bug阿斯顿发送到发生","status":"待解决","endTime":"2016-10-29","url":"bug/detail?id=14816&p=32"},
-                {"id":"14816","urgent":"","name":"外包bug阿斯顿发送到发生","status":"待解决","endTime":"2016-10-29","url":"bug/detail?id=14816&p=32"},
-                {"id":"14816","urgent":"","name":"外包bug阿斯顿发送到发生","status":"待解决","endTime":"2016-10-29","url":"bug/detail?id=14816&p=32"},
+                {"id":"14816","urgent":"","name":"  bug添加一个bug责任人申诉的额外流程 ","status":"待解决","endTime":"2016-10-29","url":"bug/detail?id=14816&p=32"},
                 {"id":"14816","urgent":"","name":"外包bug阿斯顿发送到发生","status":"待解决","endTime":"2016-10-29","url":"bug/detail?id=14816&p=32"}
             ]
         }
